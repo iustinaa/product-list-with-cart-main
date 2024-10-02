15a25282cdc7f563d74178ec26ee3b3b5a157497
Iustina Bucur <bucur.iustina@outlook.com>
2024-10-02 22:17:11 +0200

    product component

---
+++ src/components/productItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <article class="products__list__item">
+      <div class="products__list__item-image">
+        <img :src="mobileImage" :srcset="`${mobileImage} 769w, ${tabletImage} 992w, ${desktopImage} 1200w`" alt="Waffle with Berries">
+        <button><img src="../assets/images/icon-add-to-cart.svg" alt="Add to Cart"><span>Add to Cart</span></button>
+      </div>
+
+      <div class="products__list__item-category red-hat-text-regular">{{props.category}}</div>
+        <h2 class="products__list__item-title red-hat-text-semibold">{{props.name}}</h2>
+      <div class="products__list__item-price red-hat-text-bold">${{props.price}}</div>
+    </article>
+</template>
+<script setup>
+const props = defineProps(['image','name', 'price', 'category'])
+console.log(props.image);
+
+const mobileImage = new URL(props.image.mobile, import.meta.url);
+const tabletImage = new URL(props.image.tablet, import.meta.url);
+const desktopImage = new URL(props.image.desktop, import.meta.url);
+</script>
