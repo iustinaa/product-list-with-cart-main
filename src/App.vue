98c5757f990fc8cc3e31063d7a8a19bbcaa25f26
Iustina Bucur <bucur.iustina@outlook.com>
2024-10-03 13:05:37 +0200

    add the products in cart

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import productItem from "@/components/productItem.vue";
 import rahat from "@/data.json"; // this automatically converts a json to a js array/object
 
-  const products = ref(rahat);
+const products = ref(rahat);
+const cart = reactive([]); // new empty reactive array for vue
 
-  console.log();
+  // Define a reactive variable for the cart count
+const cartCount = ref(0);
+
+// Define a method to increment the cart count
+const addToCart = (name, price) => {
+  // console.log(name, price)
+  cartCount.value++;
+  cart.push([name, price]);
+  console.log(cart);
+};
+
+const removeFromCart = (product) => {
+  const index = this.cart.indexOf(product);
+    if (index > -1) {
+      this.cart.splice(index, 1);
+    }
+}
+
+  // console.log();
 
 </script>
 
@@ -15,12 +34,24 @@ import rahat from "@/data.json"; // this automatically converts a json to a js a
       <h1 class="red-hat-text-bold">Desserts</h1>
       <div>
         <!-- it goes for each product from the json and renders the component , key is a vue magic thing to make sure reactivity / rendering works without issue -->
-        <productItem v-for = "product in products" :key="product.name" :image="product.image"
-        :category="product.category" :name="product.name" :price="product.price" />
+        <productItem v-for = "product in products" 
+          :key="product.name"
+          :image="product.image"
+          :category="product.category" 
+          :name="product.name"
+          :price="product.price"
+          @addToCart="addToCart"
+          />
       </div>
     </main>
     <aside class="products__cart">
-        <h1 class="red-hat-text-bold">Your Cart(0)</h1>
+        <h1 class="red-hat-text-bold">Your Cart({{ cartCount }})</h1>
+        <ul>
+          <li v-for="product in cart" :key="product[0]" @addToCart="addToCart">
+            {{ product[0]}} - {{ product[1]}}
+            <button @click="removeFromCart(product)">Remove</button>
+          </li>
+        </ul>
         <div>
           <img src="./assets/images/illustration-empty-cart.svg" alt="Empty cart">
           <p class="red-hat-text-semibold">Your added items will appear here</p>
--- src/components/productItem.vue
+++ src/components/productItem.vue
@@ -2,7 +2,7 @@
     <article class="products__list__item">
       <div class="products__list__item-image">
         <img :src="mobileImage" :srcset="`${mobileImage} 769w, ${tabletImage} 992w, ${desktopImage} 1200w`" alt="Waffle with Berries">
-        <button><img src="../assets/images/icon-add-to-cart.svg" alt="Add to Cart"><span>Add to Cart</span></button>
+        <button @click="$emit('addToCart', props.name, props.price)"><img src="../assets/images/icon-add-to-cart.svg" alt="Add to Cart"><span>Add to Cart</span></button>
       </div>
 
       <div class="products__list__item-category red-hat-text-regular">{{props.category}}</div>
@@ -12,9 +12,17 @@
 </template>
 <script setup>
 const props = defineProps(['image','name', 'price', 'category'])
-console.log(props.image);
+// console.log(props.image);
 
 const mobileImage = new URL(props.image.mobile, import.meta.url);
 const tabletImage = new URL(props.image.tablet, import.meta.url);
 const desktopImage = new URL(props.image.desktop, import.meta.url);
+
+// let cartQuantityValue = 0;
+// function cartQuantity() {
+//   cartQuantityValue = cartQuantityValue + 1;
+//   console.log(`Cart quantity: ${cartQuantityValue}`)
+// }
+
+
 </script>
